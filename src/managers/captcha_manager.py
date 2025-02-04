20d07112db8db371fbd5885183ff19c5fc995abf
usrofgh <anon@gmail.com>
2025-02-04 23:00:13 +0300

    handle an error of captcha waiting

--- src/managers/captcha_manager.py
+++ src/managers/captcha_manager.py
@@ -23,5 +23,8 @@ class CaptchaManager:
             self._curr_solver += 1
 
         time.sleep(random.uniform(1, 3))
-        code = solver.hcaptcha(sitekey=self._site_key, url=self._base_url)["code"]
+        try:
+            code = solver.hcaptcha(sitekey=self._site_key, url=self._base_url)["code"]
+        except Exception:
+            return
         self.captcha_token_pool.append(code)
